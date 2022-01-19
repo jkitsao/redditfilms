868d246c3acfe0aace6456539ac5299e43a5a224
kitsao <kitsaojackson22@gmail.com>
2022-01-19 17:42:23 +0300

    change OG image to custom one

--- components/Helmet.js
+++ components/Helmet.js
@@ -10,19 +10,21 @@ function Helmet({ title }) {
   const siteName = "redditmovies";
   return (
     <div className="bg-gray-900 h-0">
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      + <meta name="description" content={description} />
-      <title>{pageTitle}</title>
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary" key="twcard" />
-      <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
-      {/* Open Graph */}
-      <meta property="og:url" content={currentURL} key="ogurl" />
-      <meta property="og:image" content={previewImage} key="ogimage" />
-      <meta property="og:site_name" content={siteName} key="ogsitename" />
-      <meta property="og:title" content={pageTitle} key="ogtitle" />
-      <meta property="og:description" content={description} key="ogdesc" />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <title>{pageTitle}</title>
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
+        {/* Open Graph */}
+        <meta property="og:url" content={currentURL} key="ogurl" />
+        <meta property="og:image" content={previewImage} key="ogimage" />
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+      </Head>
     </div>
   );
 }
