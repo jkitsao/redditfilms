69f6afa358c7a10da4e363620f42ca07210cbbcb
kitsao <kitsaojackson22@gmail.com>
2023-04-27 22:58:33 +0300

    feat: add kformatter for large values on upvote component

--- components/ThreadComponent.js
+++ components/ThreadComponent.js
@@ -4,8 +4,9 @@ import { useRouter } from "next/router";
 import CommentsThreadComp from "./CommentsThreadComp";
 // import { Anchorme } from "react-anchorme";
 import ReactMarkdown from "react-markdown";
-import parse from "html-react-parser";
-import { motion, AnimatePresence } from "framer-motion"
+import remarkGfm from "remark-gfm";
+// import parse from "html-react-parser";
+import { motion, AnimatePresence } from "framer-motion";
 import ToggleComp from "./ToggleComp";
 import PreviewComp from "./PreviewComp";
 import ProfileComp from "./ProfileComp";
@@ -46,26 +47,25 @@ function MovieThreadComponent({ thread }) {
   }, [asPath]);
 
   const variants = {
-    open: { opacity: 1, height: '100%' },
+    open: { opacity: 1, height: "100%" },
     closed: { opacity: 0, height: 0 },
-    transition: { duration: 5 }
-  }
+    transition: { duration: 5 },
+  };
 
   return (
-
     <div className="bg-gray-900 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-70   pb-4 select-none shadow-md m-2 justify-between   rounded cursor-pointer  transition-all duration-150 ease-linear thread_div whitespace-pre-wrap relative ">
-      <motion.div className=" mb-3"
-
-      >
+      <motion.div className=" mb-3">
         <div className="flex items-center justify-between mb-2 p-2">
           <ProfileComp author={thread?.data?.author} />
           <Upvote thread={thread} />
         </div>
-        <div className="px-2  mt-5 ">
-          <span className=" block text-cyan-400 text-xl  my-2 font-semibold title tracking-wide">
+        <div className="px-2 prose lg:prose-2xl prose-p:font-semi-bold prose-p:text-gray-300/90  my-4 ">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {/* <span className=" block text-cyan-600 text-xl  my-2 font-semibold title tracking-wide"> */}
             {thread?.data?.title}
             {/* {getHostname(thread?.data.url)} */}
-          </span>
+            {/* </span> */}
+          </ReactMarkdown>
         </div>
         {thread.data?.thumbnail &&
           getLocation(thread?.data.url)?.hostname !== "www.reddit.com" && (
@@ -77,12 +77,18 @@ function MovieThreadComponent({ thread }) {
         {thread.data?.thumbnail &&
           getLocation(thread?.data.url)?.hostname == "youtu.be" && (
             <div>
-              <Player id={getLocation(thread?.data.url)?.pathname.substring(1)} />
+              <Player
+                id={getLocation(thread?.data.url)?.pathname.substring(1)}
+              />
             </div>
           )}
-        {thread?.data?.selftext && <div className="my-3 pb-2 markdown_div  text-gray-400 font-normal leading-snug prose prose-a:text-blue-400 prose-strong:text-green-400 prose-base hover:prose-a:text-blue-200 hover:prose-strong:text-green-200 transition-all duration-200 p-2">
-          <ReactMarkdown>{thread?.data?.selftext}</ReactMarkdown>
-        </div>}
+        {thread?.data?.selftext && (
+          <div className="my-3 pb-2 markdown_div  text-gray-400 font-normal leading-snug prose prose-a:text-blue-400 prose-strong:text-green-400 prose-base hover:prose-a:text-blue-200 hover:prose-strong:text-green-200 transition-all duration-200 p-2">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {thread?.data?.selftext}
+            </ReactMarkdown>
+          </div>
+        )}
         {isImage && (
           <div className="w-full my-3">
             <img src={thread?.data?.url} alt="" className="w-full" />
@@ -91,13 +97,14 @@ function MovieThreadComponent({ thread }) {
 
         <AnimatePresence>
           {isCommentsOpen && (
-            <motion.div className="my-2"
+            <motion.div
+              className="my-2"
               exit={{
                 height: 0,
                 transition: {
                   height: {
                     duration: 0.07,
-                    type: 'spring'
+                    type: "spring",
                   },
                 },
               }}
@@ -121,7 +128,6 @@ function MovieThreadComponent({ thread }) {
         />
       </div>
     </div>
-
   );
 }
 
--- components/Upvote.jsx
+++ components/Upvote.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function Upvote({ thread }) {
+    function kFormatter(num) {
+        return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
+    }
+    
     return (
         <div className="text-gray-600 font-bold px-3 flex justify-center items-center text-center">
             <div>
@@ -11,7 +15,7 @@ function Upvote({ thread }) {
 
                 {/* </span> */}
                 <span className='inline-flex justify-center text-xs  lg:text-sm '>
-                    {thread?.data?.ups}
+                    {kFormatter(thread?.data?.ups)}
                 </span>
             </div>
         </div>
